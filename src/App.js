9286fa22915ac0d35ab43addbfea71f19c3e05d1
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-09-28 11:46:07 +0300

    validated email and password using useState

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import Fruits from './Fruits';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Products from './Products';
 import Vegetables from './Vegetables';
+import Login from './Login';
 
 
 function App() {
   return (
     <div className="App">
-        <h2 className='title'>WELCOME TO GREENS KIOSK</h2>
-        <p className='title'>Various Fruits and Vegetables</p>
+        {/* <h2 className='title'>WELCOME TO GREENS KIOSK</h2>
+        <p className='title'>Various Fruits and Vegetables</p> */}
         <div className='kiosk'>
-          <Products/>
-          <div className='category'>
-          <Fruits/>
-          <Vegetables/>
-          </div>
+          {/* <div className='category'> */}
+            <Login/>
+          {/* </div> */}
           </div>
     </div>
   );
---
+++ src/Login.js
@@ -0,0 +1,67 @@
+import React,{useState} from "react";
+import "./login.css"
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Home from "./Home";
+
+const Login=()=>{
+
+    const[email,setEmail]=useState('');
+    console.log(email)
+    const[password,setPassword]=useState('');
+    const [errorPassword, setErrorPassword]=useState(false);
+    const [errorEmail, setErrorEmail]=useState(false);
+
+    
+    const handleSubmit=(e)=>{
+        setErrorEmail(false)
+        setErrorPassword(false)
+        e.preventDefault()
+
+        if(!/\S+@\S+\.\S+/.test(email)){
+            setErrorEmail(true)
+        }
+        if(password.length<8){    
+            setErrorPassword(true)
+            return
+        }
+        else{
+            <Router>
+               <Link to="/"><li>Home</li></Link>
+               <Route path="/" exact component = {Home}/>
+   
+            </Router>
+           }
+    }
+    return(
+        <div className="about">
+         
+            <h1>Welcome Back</h1>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label className="label">Your email</label>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input  className="input" type="email" placeholder="&nbsp;&nbsp;name@domain.com"
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                        value={email}/><br/>
+                        {errorEmail && <i style={{fontSize:"11px", color:"red"}}>Email not valid</i>}
+                    </div>
+                    <div>
+                        <label className="label">Password</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input className="input" type="password" placeholder="&nbsp;&nbsp;at least 8 characters"
+                        onChange={(e)=>{setPassword(e.target.value)}} 
+                        value={password}/> <br/>
+                        {errorPassword && <i style={{fontSize:"11px", color:"red"}}>Password must be atleast eight characters</i>}
+                    </div>
+                    <div>
+                    <input type={"checkbox"}/><label>Keep me logged in</label><span className="forget">Forgot password?</span>
+                    </div>
+
+                    <button>Login</button><br/>
+                    <div className="or"><hr/><span>Or</span><hr/></div>
+
+                </form>
+             
+            </div>
+         
+         )
+};
+export default Login
